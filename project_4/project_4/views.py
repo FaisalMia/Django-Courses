60af47310bcbac513a997b8b1a7e88a2adef506f
FaisalMia <faisalmia369@gmail.com>
2024-08-14 11:22:54 +0600

    django templating code

---
+++ project_4/project_4/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# D:\setup\phitron batch-4\Django\Django Course(folder,directory)\project_4\templates\index.html
+def index(request):
+    return render(request,'index.html')
+
+
